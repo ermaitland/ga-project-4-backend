56e675db2fd7d7e31f99385472956f0b6490ae55
ermaitland <maitland.eleanor@gmail.com>
2023-01-26 16:14:57 +0000

    django cors headers installed

--- project/settings.py
+++ project/settings.py
@@ -44,16 +44,19 @@ INSTALLED_APPS = [
     'requests',
     'FAQs',
     'my_meds',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',  
 ]
 
 ROOT_URLCONF = 'project.urls'
