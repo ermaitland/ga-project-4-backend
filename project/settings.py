398b6da3a7760f3d4ac676d05280ceecabb8e32c
ermaitland <maitland.eleanor@gmail.com>
2023-01-27 09:05:09 +0000

    deploying

--- project/settings.py
+++ project/settings.py
@@ -157,6 +157,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://medichecker.netlify.app",
 ]
 
 django_on_heroku.settings(locals())
